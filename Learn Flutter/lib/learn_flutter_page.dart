1015931a473f056542a56ba0eaf1ad3b65fe2e9b
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2022-10-20 01:07:35 +0700

    test

--- Learn Flutter/lib/learn_flutter_page.dart
+++ Learn Flutter/lib/learn_flutter_page.dart
@@ -11,16 +11,49 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Learn Flutter'),
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(Icons.arrow_back_ios),
+        appBar: AppBar(
+          title: Text('Learn Flutter'),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back_ios),
+          ),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Image.asset('images/asuka langley.jpg'),
+            SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin:  const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: Center(
+                child: const Text(
+                  "This is text widget",
+                  style: TextStyle(color: Colors.white60),
+                ),
+              ),
+              
+            ),
+            ElevatedButton(
+              onPressed: () {
+              debugPrint("Elevated Button");
+            }, 
+            child: const Text("Elevated Button")),
+            ElevatedButton(
+              onPressed: () {
+              debugPrint("Elevated Button");
+            }, 
+            child: const Text("Elevated Button"))
+          ],
+        ));
   }
 }
